bec2cb00c419c7feaa6b46d6892f4889e4714427
Pepe Doval <pepellou@gmail.com>
2012-12-15 12:17:09 +0100

    Parse all kind of numbers

--- test/InterpreterTest.php
+++ test/InterpreterTest.php
@@ -5,7 +5,23 @@ class Interpreter {
 	public function evaluate(
 		$input
 	) {
+		if ($this->isVariable($input)) {
+			throw new Exception();
+		}
 		return $input;
+	}
+
+	private function isVariable(
+		$input
+	) {
+		return !$this->isNumber($input);
+	}
+
+	private function isNumber(
+		$input
+	) {
+		$parser = new Parser();
+		return $parser->isNumber($input);
 	}
 
 }
@@ -34,6 +50,30 @@ class InterpreterTest extends PHPUnit_Framework_TestCase {
 			"Negative" => array("-1"),
 			"Zero" => array("0"),
 			"Float number" => array("1.2"),
+		);
+	}
+
+	/**
+	* @dataProvider variable_names
+	* @expectedException Exception
+	*/
+	public function test_variableEvaluatesErrorIfNotExist(
+		$aVariable
+	) {
+		$interpreter = new Interpreter();
+		
+		$interpreter->evaluate($aVariable);
+	}
+
+	public static function variable_names(
+	) {
+		return array(
+			"sample variable name" => array("x"),
+			"letters & numbers" => array("x1"),
+			"identifier starting with - (a)" => array("-a"),
+			"identifier starting with - (b)" => array("-12a"),
+			"upcase" => array("X"),
+			"any length" => array("asjdghadsgdfk"),
 		);
 	}
 
---
+++ test/ParserTest.php
@@ -0,0 +1,80 @@
+<?php
+
+class Parser {
+
+	public function isNumber(
+		$input
+	) {
+		$valid_chars = array(
+			'+', '-', 'e', 'E', '.',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+		);
+		for ($i = 0; $i < strlen($input); $i++) {
+			if (!in_array($input[$i], $valid_chars)) {
+				return false;
+			}
+		}
+		return preg_match("/^[+-]?[0-9]*\.[0-9]*([eE][+-]?[0-9]+)?$|^[+-]?[0-9]+([eE][+-]?[0-9]+)?$/", $input) != false;
+	}
+
+}
+
+class ParserTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	* @dataProvider numbers
+	*/
+	public function test_isNumber(
+		$aNumber
+	) {
+		$parser = new Parser();
+
+		$this->assertTrue($parser->isNumber($aNumber));
+	}
+
+	public static function numbers(
+	) {
+		return array(
+			"Sample number (a)" => array("1"),
+			"Sample number (b)" => array("12"),
+			"Negative" => array("-1"),
+			"Explicit positive" => array("+1"),
+			"Zero" => array("0"),
+			"- Zero" => array("-0"),
+			"+ Zero" => array("+0"),
+			"Float number" => array("1.2"),
+			"Scientific notation (a)" => array("1.2e12"),
+			"Scientific notation (b)" => array("1.2e+12"),
+			"Scientific notation (c)" => array("1.2e-12"),
+			"Ending with point" => array("12."),
+			"Starting with point" => array(".12"),
+		);
+	}
+
+	/**
+	* @dataProvider not_numbers
+	*/
+	public function test_isNumber_fail(
+		$aNumber
+	) {
+		$parser = new Parser();
+
+		$this->assertFalse($parser->isNumber($aNumber));
+	}
+
+	public static function not_numbers(
+	) {
+		return array(
+			"sample variable name" => array("x"),
+			"letters & numbers" => array("x1"),
+			"identifier starting with - (a)" => array("-a"),
+			"identifier starting with - (b)" => array("-12a"),
+			"upcase" => array("X"),
+			"any length" => array("asjdghadsgdfk"),
+			"bad syntax (a)" => array("-12e"),
+			"bad syntax (b)" => array("-1..2"),
+			"bad syntax (c)" => array("1-2"),
+		);
+	}
+
+}
